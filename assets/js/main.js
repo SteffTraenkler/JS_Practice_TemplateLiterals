3605d752bf861bedbdc2e33c4330e2bd28f27021
SuperCoder <supercoder@MacBook-Air-von-SuperCoder-2.local>
2022-02-09 14:48:49 +0100

    duuude

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+console.log("Lev1_1");
+let firstName = "Ich";
+let lastName = "hasse";
+let aGe = "es";
+let birthPlace = "meine";
+let heiGth = "persönlichen";
+let weiGth = "Daten";
+let hobbIes = "irgendwo";
+let gamEs = "angeben";
+let favouriteFood = "zu";
+let favouriteSports = "müssen";
+let favouriteBrand = "!!!";
+
+var x = document.getElementById("personalShi");
+
+x.innerHTML = `${firstName} ${lastName} ${aGe} ${birthPlace} ${heiGth} ${weiGth} ${hobbIes} ${gamEs} ${favouriteFood} ${favouriteSports} ${favouriteBrand} <br> <br> <br> <br>`
+console.log(`${firstName} ${lastName} ${aGe} ${birthPlace} ${heiGth} ${weiGth} ${hobbIes} ${gamEs} ${favouriteFood} ${favouriteSports} ${favouriteBrand}`);
+
+console.log(" ");
+console.log(" ");
+console.log("Lev1_2");
+
+let y = document.getElementById("madness");
+
+let lunatic = "lunatic is";
+let darkSide = "I'll see you on the dark side of the moon";
+let hall = "hall";
+let head = "in my head";
+let door = "You lock the door";
+let paperboy = "paperboy";
+let and = 'And';
+let the = 'The';
+
+y.innerHTML = `Brain Damage Songtext<br>
+${the} ${lunatic} on the grass<br>
+${the} ${lunatic} on the grass<br>
+Remembering games and daisy chains and laughs<br>
+Got to keep the loonies on the path<br>
+<br>
+${the} ${lunatic} in the ${hall}<br>
+${the} lunatics are in my ${hall}<br>
+${the} paper holds their folded faces to the floor<br>
+${and} every day the ${paperboy} brings more<br>
+<br>
+<br>
+${and} if the dam breaks open many years too soon<br>
+${and} if there is no room upon the hill<br>
+${and} if your head explodes with dark forebodings too<br>
+${darkSide}<br>
+<br>
+${the} ${lunatic} ${head}<br>
+${the} ${lunatic} ${head}<br>
+You raise the blade, you make the change<br>
+You rearrange me 'til I'm sane<br>
+${door}<br>
+${and} throw away the key<br>
+${the}re's someone ${head} but it's not me<br>
+<br>
+${and} if the cloud bursts, thunder in your ear<br>
+You shout and no one seems to hear<br>
+${and} if the band you're in starts playing different tunes<br>
+${darkSide}<br>
+<br>`
+
